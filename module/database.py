3328ec129162bb45e56d7cf348f4b40cc6099583
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2025-05-20 23:21:23 +0800

    v1.0.2 修复数据库对user_type的默认值设定。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- module/database.py
+++ module/database.py
@@ -170,7 +170,7 @@ class MySQLDatabase:
                 gender VARCHAR(50) NOT NULL,
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                user_type TINYINT DEFAULT 0 NOT NULL,  -- 0=普通用户, 1=管理员
+                user_type TINYINT DEFAULT 1 NOT NULL,  -- 0=管理员, 1=普通用户
                 photo_path VARCHAR(255) NOT NULL,
                 face_meta JSON NOT NULL,
                 create_time DATETIME NOT NULL
